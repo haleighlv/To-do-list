f49e80b5aff75d56eb52852558cf1fd17d05949c
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-11-22 10:40:44 +1000

    fixing app

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,26 +1,19 @@
 import './style.css'
 
 function TodoItem(props) {
+    const { todo, completeTodo, index, removeTodo } = props; 
     return (
-        <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
-            {props.todo.text}
+        <div className={`todo ${todo.isCompleted ? "complete" : ""}`}>
+            {todo.text}
             <div>
-                <button onClick={() => props.completeTodo(props.index)}>Complete</button>
+                <button onClick={() => props.completeTodo(index)}>Complete</button>
             </div>
             <div>
-                <button onClick={() => props.removeTodo(props.index)}>x</button>
+                <button onClick={() => props.removeTodo(index)}>Remove</button>
             </div>
         </div>
     );
 }
 
-function removeTodo(props) {
-    <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
-        {props.todo.text}
-        <div>
-            <button onClick={() => props.removeTodo(props.index)}>x</button>
-        </div>
-    </div>
-}
 
     export default TodoItem;
